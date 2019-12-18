90d67eb0c2d559cb1ed7e757df6912081feb467b
ThierrySquirrel <2161271173@qq.com>
2019-12-18 17:26:46 +0800

    update 2.0.9-RELEASE

--- rocketmq-spring-boot-autoconfigure/src/main/java/com/github/thierrysquirrel/core/serializer/RocketSerializer.java
+++ rocketmq-spring-boot-autoconfigure/src/main/java/com/github/thierrysquirrel/core/serializer/RocketSerializer.java
@@ -25,20 +25,21 @@ package com.github.thierrysquirrel.core.serializer;
  * @since JDK 1.8
  */
 public interface RocketSerializer {
-    /**
-     * 序列化为byte[]
-     *
-     * @param object 对象
-     * @return 二进制数据
-     */
-    <T> byte[] serialize(T object);
+	/**
+	 * 序列化为byte[]
+	 * @param object 对象
+	 * @param <T> 泛型
+	 * @return 二进制数据
+	 */
+	<T> byte[] serialize(T object);
 
-    /**
-     * byte[]反序列化为对象
-     *
-     * @param bytes 序列化的二进制数据
-     * @param clazz 反序列化后的对象
-     * @return 对象
-     */
-    <T> T deSerialize(byte[] bytes, Class<T> clazz);
+	/**
+	 * byte[]反序列化为对象
+	 *
+	 * @param bytes 序列化的二进制数据
+	 * @param clazz 反序列化后的对象
+	 * @param <T>   T
+	 * @return 对象
+	 */
+	<T> T deSerialize(byte[] bytes, Class<T> clazz);
 }
